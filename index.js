505d10e0957a2f7e5b58909a7ece1f89c6ae2eee
Woody Evins <woodyevins@Woodys-MacBook-Pro.local>
2020-05-25 13:21:47 -0400

    update to simple html repo

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const el = document.querySelector('p.text-field');
 bpMonitor.addEventListener('animationstart', () => {
     const currentBreakpoint = window.getComputedStyle(bpMonitor, ':after').content;
     el.innerText = currentBreakpoint;
-
 })
 
